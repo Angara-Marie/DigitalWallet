515c1dc2385e1229c1f25e43af63a86965258535
Tessa <angaramarie@gmail.com>
2022-10-27 10:38:36 +0300

    added wallet functionality

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
+from . import views
 
 from api.serializers import ReceiptSerializer
 from .views import AccountViewSets, CardViewSets, CustomerViewSets, LoanViewSets, NotificationsViewSets, ReceiptViewSets, TransactionViewSets, WalletViewSets
@@ -13,6 +14,14 @@ router.register(r"transactions", TransactionViewSets)
 router.register(r"loans", LoanViewSets)
 router.register(r"receipts", ReceiptViewSets)
 router.register(r"notifications", NotificationsViewSets)
+
+
 urlpatterns= [
-    path("", include(router.urls))
+    path("", include(router.urls)),
+    path("deposit/", views.AccountDepositView.as_view(), name="deposit-view"),
+    path("withdraw/", views.AccountWithdrawView.as_view(), name="withdraw-view"),
+    path("transfer/", views.AccountTransferView.as_view(), name="transfer-view"),
+    path("request_loan/", views.AccountRequestLoanView.as_view(), name="request-loan-view"),
+    path("repay_loan/", views.AccountRepayLoanView.as_view(), name="repay-loan-view"),
+    path("buy_airtime/", views.AccountBuyAirtimeView.as_view(), name="buy-airtime-view"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,39 +1,140 @@
 from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import viewsets
-from wallet.models import Account, Card, Customer, Notifications, Receipt, Transaction, Wallet,Loan
+from rest_framework import views
+from rest_framework.response import Response
+from wallet.models import Account, Card, Customer, Notifications, Receipt, Transaction, Wallet, Loan
 from .serializers import AccountSerializer, CardSerializer, CustomerSerializer, LoanSerializer, NotificationsSerializer, ReceiptSerializer, TransactionSerializer, WalletSerializer
 
 # Create your views here.
 
+
 class CustomerViewSets(viewsets.ModelViewSet):
     queryset = Customer.objects.all()
-    serializer_class= CustomerSerializer
+    serializer_class = CustomerSerializer
+
 
 class WalletViewSets(viewsets.ModelViewSet):
     queryset = Wallet.objects.all()
     serializer_class = WalletSerializer
 
+
 class AccountViewSets(viewsets.ModelViewSet):
     queryset = Account.objects.all()
-    serializer_class = AccountSerializer 
+    serializer_class = AccountSerializer
+
+
+class AccountDepositView(views.APIView):
+   """
+   This class allows deposit of funds to an account.
+   Accepts this JSON data
+   {
+       "account_id": 123,
+       "amount": 1000
+   }
+   This API needs Authentication and Permissions to be added
+   """
+   def post(self, request, format=None):       
+       account_id = request.data["account_id"]
+       amount = request.data["amount"]
+       try:
+           account = Account.objects.get(id=account_id)
+       except ObjectDoesNotExist:
+           return Response("Account Not Found", status=404)
+      
+       message, status = account.deposit(amount)
+       return Response(message, status=status)
+
+class AccountWithdrawView(views.APIView):
+   """
+   This class allows withdraw of funds to an account.
+   Accepts this JSON data
+   {
+       "account_id": 123,
+       "amount": 1000
+   }
+   This API needs Authentication and Permissions to be added
+   """
+   def post(self, request, format=None):       
+       account_id = request.data["account_id"]
+       amount = request.data["amount"]
+       try:
+           account = Account.objects.get(id=account_id)
+       except ObjectDoesNotExist:
+           return Response("Account Not Found", status=404)
+      
+       message, status = account.withdraw(amount)
+       return Response(message, status=status)
+class AccountTransferView(views.APIView):
+   def post(self, request, format=None):       
+       account_id = request.data["account_id"]
+       amount = request.data["amount"]
+       try:
+           account = Account.objects.get(id=account_id)
+       except ObjectDoesNotExist:
+           return Response("Account Not Found", status=404)
+      
+       message, status = account.transfer(amount)
+       return Response(message, status=status)
+
+class AccountRequestLoanView(views.APIView):
+   def post(self, request, format=None):       
+       account_id = request.data["account_id"]
+       amount = request.data["amount"]
+       try:
+           account = Account.objects.get(id=account_id)
+       except ObjectDoesNotExist:
+           return Response("Account Not Found", status=404)
+      
+       message, status = account.request_loan(amount)
+       return Response(message, status=status) 
+
+class AccountRepayLoanView(views.APIView):
+   def post(self, request, format=None):       
+       account_id = request.data["account_id"]
+       amount = request.data["amount"]
+       try:
+           account = Account.objects.get(id=account_id)
+       except ObjectDoesNotExist:
+           return Response("Account Not Found", status=404)
+      
+       message, status = account.repay_loan(amount)
+       return Response(message, status=status)  
+
+class AccountBuyAirtimeView(views.APIView):
+   def post(self, request, format=None):       
+       account_id = request.data["account_id"]
+       amount = request.data["amount"]
+       try:
+           account = Account.objects.get(id=account_id)
+       except ObjectDoesNotExist:
+           return Response("Account Not Found", status=404)
+      
+       message, status = account.buy_airtime(amount)
+       return Response(message, status=status)     
+
 
 class CardViewSets(viewsets.ModelViewSet):
     queryset = Card.objects.all()
     serializer_class = CardSerializer
 
+
 class TransactionViewSets(viewsets.ModelViewSet):
     queryset = Transaction.objects.all()
-    serializer_class = TransactionSerializer  
+    serializer_class = TransactionSerializer
+
 
 class LoanViewSets(viewsets.ModelViewSet):
     queryset = Loan.objects.all()
     serializer_class = LoanSerializer
 
+
 class ReceiptViewSets(viewsets.ModelViewSet):
     queryset = Receipt.objects.all()
     serializer_class = ReceiptSerializer
 
+
 class NotificationsViewSets(viewsets.ModelViewSet):
     queryset = Notifications.objects.all()
-    serializer_class = NotificationsSerializer        
+    serializer_class = NotificationsSerializer
 
